e3c9109144f6c24c288d93c2d0142a4569b681be
Veronica K. B. Olsen <1619840+vkbo@users.noreply.github.com>
2020-08-03 15:44:21 +0200

    Add license statement to source files, and added API class

--- sentinel/__init__.py
+++ sentinel/__init__.py
@@ -6,20 +6,20 @@
  Package initialisation
 
  This file is a part of Sentinel Tools
- Copyright 2020, Veronica Berglyd Olsen and MET Norway
 
- This program is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
+ Copyright 2020 - MET Norway (Machine Ocean Project)
 
- This program is distributed in the hope that it will be useful, but
- WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- General Public License for more details.
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
 
- You should have received a copy of the GNU General Public License
- along with this program. If not, see <https://www.gnu.org/licenses/>.
+    http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
 """
 
 import os
@@ -27,7 +27,7 @@ import logging
 
 __package__    = "sentinel"
 __author__     = "Veronica Berglyd Olsen"
-__copyright__  = "Copyright 2020, Veronica Berglyd Olsen and MET Norway"
+__copyright__  = "Copyright 2020, MET Norway (Machine Ocean Project)"
 __license__    = "GPLv3"
 __maintainer__ = "Veronica Berglyd Olsen"
 __email__      = "vkbo@met.no"
@@ -37,6 +37,11 @@ __credits__    = [
 ]
 
 from .config import Config
+from .apiwrapper import SentinelAPI
+
+__all__ = [
+    "Config", "SentinelAPI"
+]
 
 # Initiating logging
 strLevel = os.environ.get("SENTINEL_LOGLEVEL", "INFO")
---
+++ sentinel/apiwrapper.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""Sentinel Tools API Class
+
+ Sentinel Tools - API Class
+============================
+ Wrapper class for the Sentinel (Copernicus) API
+
+ This file is a part of Sentinel Tools
+
+ Copyright 2020 - MET Norway (Machine Ocean Project)
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+"""
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+class SentinelAPI():
+
+    def __init__(self):
+
+        return
+
+# END Class SentinelAPI
